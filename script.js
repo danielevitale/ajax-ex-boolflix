663e2da6e9db1d3d16c09beab6a195b65a5a4b86
danielevitale <danvitale@outlook.it>
2019-05-28 19:07:56 +0200

    Stelle e bandiere OK

--- script.js
+++ script.js
@@ -7,53 +7,72 @@
 // 1. Titolo 2. Titolo Originale 3. Lingua 4. Voto
 
 
+//---------------------------------FUNZIONI------------------------------------
+
+// Funzione che genera il numero di stelle (piene e vuote) dato in input il voto ottenuto dall'API
+function genera_stelle(voto) {
+  // Trasformo il voto in un umero compreso tra 0 e 5
+  var voto_arrotondato = Math.ceil((voto / 2));
+  console.log(voto_arrotondato);
+  // Inizializzo a 0 la var che conterrà le stringhe delle stelle
+  var stelle = '';
+  // Creo la stringa che conterrà le stelle piene (concatenando il loro html)
+  for (var j = 1; j <= voto_arrotondato; j++) {
+    stelle += '<i class="fas fa-star"></i>';
+  }
+  var voto_massimo = 5;
+  // Aggiungo alla stringa delle stelle piene le stelle vuote (concatenando il loro html)
+  for (var z = 1; z <= (voto_massimo - voto_arrotondato); z++) {
+    stelle += '<i class="far fa-star"></i>';
+  }
+  return stelle
+}
+
+
+// Funzione che genera la bandiera della nazione dato in input la lingua originale
+function genera_bandiere(lingua) {
+  // Inizializzo la var flag come stringa vuota
+  var flag = '';
+  // Assegno l'img della bandiera corrispondente nei casi di lingua italiano, inglese, spagnolo e francese;
+  // negli altri casi assegno il la stringa restituitami dall'api
+  switch(lingua) {
+    case 'it':
+      flag = '<img src="imgflag/italy.png" alt="">'
+      break;
+    case 'en':
+      flag = '<img src="imgflag/united-kingdom.png" alt="">'
+      break;
+    case 'es':
+      flag = '<img src="imgflag/spain.png" alt="">'
+      break;
+    case 'fr':
+      flag = '<img src="imgflag/france.png" alt="">'
+      break;
+    default:
+      flag = lingua
+  }
+  return flag
+}
+
+
 // Funzione che genera una card con i valori del film e la inserisce nel html
 // atraverso l'utilizzo di handlebars
 function film_card (info) {
   var source = $("#template_film").html();
   var template = Handlebars.compile(source);
-
+  // Copio nella var risultati il data ottenuto dalla chiamata api
   var risultati = info.results;
-  var film_trovati = risultati.length;
-  for (var i = 0; i < film_trovati; i++) {
-    // Trasformo il voto in un umero compreso tra 0 e 5
-    var voto_arrotondato = Math.ceil((risultati[i].vote_average / 2));
-    console.log(voto_arrotondato);
-    // Inizializzo a 0 la var che conterrà le stringhe delle stelle
-    var stelle = '';
-    // Creo la stringa che conterrà le stelle piene (concatenando il loro html)
-    for (var j = 1; j <= voto_arrotondato; j++) {
-      stelle += '<i class="fas fa-star"></i>';
-    }
-    var voto_massimo = 5;
-    // Aggiungo alla stringa delle stelle piene le stelle vuote (concatenando il loro html)
-    for (var z = 1; z <= (voto_massimo - voto_arrotondato); z++) {
-      stelle += '<i class="far fa-star"></i>';
-    }
-
-    var flag = '';
-    switch(risultati[i].original_language) {
-      case 'it':
-        flag = '<img src="imgflag/italy.png" alt="">'
-        break;
-      case 'en':
-        flag = '<img src="imgflag/united-kingdom.png" alt="">'
-        break;
-      case 'es':
-        flag = '<img src="imgflag/spain.png" alt="">'
-        break;
-      case 'fr':
-        flag = '<img src="imgflag/france.png" alt="">'
-        break;
-      default:
-        flag = risultati[i].original_language
-    }
-
+  // Scorro tutti i film restituiti dall'api e per ognuno genero una card con i rispettivi valori
+  for (var i = 0; i < risultati.length; i++) {
+    var voto_medio = risultati[i].vote_average;
+    var linguaggio = risultati[i].original_language;
+    // Genero l'oggetto da passare al template. Per la lingua e il voto richiamo le funzioni create e gli
+    // passo i valori ottenuti dall'api
     var context = {
       titolo: risultati[i].title,
       titolo_originale: risultati[i].original_title,
-      lingua: flag,
-      voto: stelle
+      lingua: genera_bandiere(linguaggio),
+      voto: genera_stelle(voto_medio)
     }
     // Genero l'html del handlebars e lo inserisco nel contenitore delle film-cards
     var html = template(context);
@@ -61,13 +80,16 @@ function film_card (info) {
   }
 }
 
+
+// URL Api base
+var url_api = 'https://api.themoviedb.org/3/'
 // Funzione che richiama l'API e genera una card con i valori restituiti
 function chiamata_api() {
   // Leggo il valore digitato dall'utente
   var digitato = $('input').val();
   // Richiamo l'API
   $.ajax({
-    url: 'https://api.themoviedb.org/3/search/movie',
+    url: url_api + 'search/movie',
     method: 'GET',
     data : {
       api_key : '204fdbaa01b03d5fdaf748fd992c0eb7',
@@ -84,6 +106,9 @@ function chiamata_api() {
   })
 }
 
+
+
+//------------------------------MAIN----------------------------------------
 
 $(document).ready(function() {
 
