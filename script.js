ff2fb48de9aa660e904e3916b588de26877b84e7
danielevitale <danvitale@outlook.it>
2019-05-30 23:38:41 +0200

    Fine esercizio

--- script.js
+++ script.js
@@ -86,8 +86,8 @@ function card (info) {
     var linguaggio = info[i].original_language;
     // Genero l'url per ottenere la copertina aggiungendo all'url di base (più la dimensione w154)
     // ciò che mi restituisce l'api
-    var url_copertina = 'https://image.tmdb.org/t/p/w154' + info[i].poster_path;
-    // Verififco se la chiamata api mi ha restiuto un url di una copertina o se invece la copertina
+    var url_copertina = 'https://image.tmdb.org/t/p/w342' + info[i].poster_path;
+    // Verififco se la chiamata api mi abbia restiuto un url di una copertina o se invece la copertina
     // non è disponibile (.poster_path = null). Se non lo è verrà visualizzata un img che indica all'utente
     // che la copertia non è disponibile
     var copertina;
@@ -95,6 +95,13 @@ function card (info) {
       copertina = '<img src="imgcopertina/copertina_non_disponibile.jpg" alt="">';
     }else {
       copertina = '<img src="'+ url_copertina + '" alt="">';
+    }
+    // Effettuo la stessa verifica per l'overview. Nel caso in cui l'api mi restituisce una overview vuota
+    // l'utente legge "Non disponibile"
+    if (info[i].overview === '') {
+      overview = 'Non disponibile';
+    }else {
+      overview = info[i].overview;
     }
     // Genero l'oggetto da passare al template. Per la lingua e il voto richiamo le funzioni create e gli
     // passo i valori ottenuti dall'api.
@@ -109,7 +116,8 @@ function card (info) {
         lingua: genera_bandiere(linguaggio),
         voto: genera_stelle(voto_medio),
         tipo: 'Film',
-        img: copertina
+        img: copertina,
+        overview : overview
       }
     }else {
       console.log('serie');
@@ -119,12 +127,13 @@ function card (info) {
         lingua: genera_bandiere(linguaggio),
         voto: genera_stelle(voto_medio),
         tipo: 'Serie tv',
-        img: copertina
+        img: copertina,
+        overview : overview
       }
     }
     // Genero l'html del handlebars e lo inserisco nel contenitore delle film-cards
     var html = template(context);
-    $('.film_container').append(html);
+    $('.container_film').append(html);
   }
 }
 
@@ -185,16 +194,28 @@ $(document).ready(function() {
   // Al click sul button parte la ricerca
   $("button").click(function(){
     // Cancello le cards della ricerca precedente
-    $('.film_container').empty();
+    $('.container_film').empty();
     chiamata_api();
   })
   // Nel digitare tasto invio parte la ricerca
   $("input").keypress(function(event){
     // Cancello le cards della ricerca precedente
-    $('.film_container').empty();
+    $('.container_film').empty();
     if (event.which == 13) {
       chiamata_api();
     }
+  })
+
+  // Quando il mouse entra sulla copertina di un film compare la card con le
+  // caratteristiche del film
+  $(document).on('mouseenter', '.card', function () {
+    $(this).find('.card_valori').slideDown(1000);
+    $(this).find('.card_copertina').hide();
+  // Quando il mouse esce dalla di un film compare la copertina del film
+  })
+  $(document).on('mouseleave', '.card', function () {
+    $(this).find('.card_valori').hide();
+    $(this).find('.card_copertina').fadeIn(1000);
   })
 
 })
